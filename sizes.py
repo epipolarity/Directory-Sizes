6d70dbd32d3906174246af0e1f519fbcc73b8a9f
James White <jwhite@gbg.co.uk>
2023-05-15 15:18:44 +0100

    add ignore directories and more verbose output

--- sizes.py
+++ sizes.py
@@ -2,7 +2,7 @@ import os
 import csv
 from tqdm import tqdm
 from configparser import ConfigParser
-
+import locale
 
 def get_directory_size(directory):
     total_size = 0
@@ -13,16 +13,16 @@ def get_directory_size(directory):
     return total_size
 
 
-def get_top_level_dir_sizes(root_directory):
+def get_top_level_dir_sizes(root_directory, ignore_directories):
     directory_list = []
     pbar = tqdm(os.listdir(root_directory), desc='Top-level Directories')
     for item in pbar:
+        pbar.set_description(f"Top-level Directory: {item}")
         item_path = os.path.join(root_directory, item)
-        if os.path.isdir(item_path):
+        if os.path.isdir(item_path) and item not in ignore_directories:
             directory_size = get_directory_size(item_path)
             directory_list.append([item, directory_size, directory_size / (1024 ** 3)])
-            pbar.set_postfix({'Size': f"{directory_size} bytes"})
-            pbar.set_description(f"Top-level Directory: {item}")
+            tqdm.write(item + ": " + '{:n}GB'.format(directory_size / (1024 ** 3)))
     return directory_list
 
 
@@ -34,12 +34,18 @@ def export_to_csv(directory_list, csv_filename):
     print(f"Directory list exported to {csv_filename} successfully!")
 
 
+locale.setlocale(locale.LC_ALL, '')
+
 # Read the root_directory from the config.ini file
 config = ConfigParser()
 config.read('config.ini')
 root_directory = config.get('Directories', 'root_directory')
+ignore_directories = config.get('Directories', 'ignore_directories').split(',')
+
+# normalise ignore directories to ensure consistent 
+ignore_directories = [directory.strip() for directory in ignore_directories]  
 
 csv_filename = "directory_sizes.csv"
 
-directory_list = get_top_level_dir_sizes(root_directory)
+directory_list = get_top_level_dir_sizes(root_directory, ignore_directories)
 export_to_csv(directory_list, csv_filename)
