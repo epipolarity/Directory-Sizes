370cb138935d6e1163f5733db3a4c681a6b47754
James White <jwhite@gbg.co.uk>
2023-05-17 16:43:39 +0100

    refactoring and improve handling of ignored dirs

--- sizes.py
+++ sizes.py
@@ -21,32 +21,35 @@ def get_directory_size(directory, pbar):
 
 
 def get_top_level_dir_sizes(root_directory, ignore_directories, check_write, min_dir_size):
-    directory_list = []
-    pbar = tqdm(os.listdir(root_directory), desc='Top-level Directories')
+    directory_sizes = []
+    _, dirnames, _ = next(os.walk(root_directory))
+    dirnames = [d for d in dirnames if d not in ignore_directories]
+    pbar = tqdm(dirnames, desc='Top-level Directories')
     for item in pbar:        
         pbar.set_description(f"Top-level Directory: {item}")
         item_path = os.path.join(root_directory, item)
-        write_access = False
-        if check_write:
-            try:
-                with open(os.path.join(item_path, '.write_test'), 'w') as test_file:
-                    write_access = True
-                os.remove(os.path.join(item_path, '.write_test'))  # Clean up the test file
-            except (PermissionError, OSError) as ex:
-                write_access = False
-        if (write_access or not check_write) and os.path.isdir(item_path) and item not in ignore_directories:
-            directory_size = get_directory_size(item_path, pbar)
-            csv_row = [item, directory_size, directory_size / (1024 ** 3)]
+        if os.path.isdir(item_path):
+            write_access = False
             if check_write:
-                csv_row.append(write_access)
-            directory_list.append(csv_row)
-            if directory_size / (1024 ** 3) > min_dir_size:
-                formatted_size = locale.format_string('%0.2f', directory_size / (1024 ** 3), grouping=True)
-                output_str = formatted_size + "GB -> " + item 
+                try:
+                    with open(os.path.join(item_path, '.write_test'), 'w') as test_file:
+                        write_access = True
+                    os.remove(os.path.join(item_path, '.write_test'))  # Clean up the test file
+                except (PermissionError, OSError) as ex:
+                    write_access = False
+            if write_access or not check_write:
+                directory_size = get_directory_size(item_path, pbar)
+                csv_row = [item, directory_size, directory_size / (1024 ** 3)]
                 if check_write:
-                    output_str += " write:{}".format(write_access)
-                tqdm.write(output_str)
-    return directory_list
+                    csv_row.append(write_access)
+                directory_sizes.append(csv_row)
+                if directory_size / (1024 ** 3) >= min_dir_size:
+                    formatted_size = locale.format_string('%0.2f', directory_size / (1024 ** 3), grouping=True)
+                    output_str = formatted_size + "GB -> " + item 
+                    if check_write:
+                        output_str += " write:{}".format(write_access)
+                    tqdm.write(output_str)
+    return directory_sizes
 
 
 def export_to_csv(directory_list, csv_filename):
