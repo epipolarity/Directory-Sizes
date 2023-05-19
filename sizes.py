3cf992afec1fee6ba3ef39a66106efe53cc7cc36
James White <jwhite@gbg.co.uk>
2023-05-19 12:35:05 +0100

    much more refactoring improved performance with multithreading additional config options

--- sizes.py
+++ sizes.py
@@ -1,87 +1,146 @@
 import os
 import csv
-from tqdm import tqdm
-from configparser import ConfigParser
 import locale
 import time
 import logging
 
-def get_directory_size(directory, pbar):
+from tqdm import tqdm
+from configparser import ConfigParser
+from concurrent.futures import ThreadPoolExecutor
+
+
+def calculate_file_size(filepath):
+    try:
+        return os.path.getsize(filepath)
+    except OSError as e:
+        logging.exception("An error occurred while calculating file size: %s", filepath)
+        return 0
+    
+
+def get_directory_size(directory, pbar, num_threads):
     total_size = 0
-    file_count = 0
-    errors = 0
     last_update = time.time()
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for file in filenames:
-            file_count += 1
-            filepath = os.path.join(dirpath, file)
-            try:
-                total_size += os.path.getsize(filepath)
-            except Exception as ex:
-                errors += 1
-                logging.error(filepath)
-                logging.exception(ex)
-            if time.time() - last_update >= 1:
-                pbar.set_postfix(update="{} files".format(file_count))
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = []
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                futures.append(executor.submit(calculate_file_size, filepath))
+                if time.time()-last_update > 1:
+                    last_update = time.time()
+                    pbar.set_postfix(update=f"Found {format_thousands(len(futures))} files")
+        
+        pbar.set_postfix(update=f"Found {format_thousands(len(futures))} files")
+        for future in futures:
+            total_size += future.result()
+            if time.time()-last_update > 1:
                 last_update = time.time()
+                pbar.set_postfix(update=f"Found {format_gigabytes(total_size)} gigabytes")
+
+    pbar.set_postfix(update=f"Found {format_gigabytes(total_size)} gigabytes")
     return total_size
 
 
-def get_top_level_dir_sizes(root_directory, ignore_directories, check_write, min_dir_size):
+def folder_writable(folder_path):
+    write_access = False
+    try:
+        with open(os.path.join(folder_path, '.write_test'), 'w') as test_file:
+            write_access = True
+        os.remove(os.path.join(folder_path, '.write_test'))  # Clean up the test file
+    except (PermissionError, OSError) as ex:
+        write_access = False
+    return write_access
+
+
+def format_thousands(num):
+    return locale.format_string('%.0f', num, grouping=True)
+
+
+def format_bytes(bytes):
+    return locale.format_string('%0.2f', bytes, grouping=True)
+
+
+def format_gigabytes(bytes):
+    return locale.format_string('%0.2f', bytes / (1024 ** 3), grouping=True)
+
+
+def get_top_level_dir_sizes(config):
+    root_directory = config['root_directory']
+    check_write = config['check_write']
     directory_sizes = []
-    _, dirnames, _ = next(os.walk(root_directory))
-    dirnames = [d for d in dirnames if d not in ignore_directories]
+    dirnames = top_level_subdir_names(root_directory, config)    
     pbar = tqdm(dirnames, desc='Top-level Directories')
-    for item in pbar:        
-        pbar.set_description(f"Top-level Directory: {item}")
-        item_path = os.path.join(root_directory, item)
-        if os.path.isdir(item_path):
+    for toplevel_subdir in pbar:        
+        pbar.set_description(f"Top-level Directory: {toplevel_subdir}")
+        full_path = os.path.join(root_directory, toplevel_subdir)
+        if os.path.isdir(full_path):
             write_access = False
             if check_write:
-                try:
-                    with open(os.path.join(item_path, '.write_test'), 'w') as test_file:
-                        write_access = True
-                    os.remove(os.path.join(item_path, '.write_test'))  # Clean up the test file
-                except (PermissionError, OSError) as ex:
-                    write_access = False
+                write_access = folder_writable(full_path)
             if write_access or not check_write:
-                directory_size = get_directory_size(item_path, pbar)
-                csv_row = [item, directory_size, directory_size / (1024 ** 3)]
-                if check_write:
-                    csv_row.append(write_access)
+                directory_size = get_directory_size(full_path, pbar, config['num_threads'])
+                csv_row = csv_data_row(config, toplevel_subdir, directory_size, write_access)
                 directory_sizes.append(csv_row)
-                if directory_size / (1024 ** 3) >= min_dir_size:
-                    formatted_size = locale.format_string('%0.2f', directory_size / (1024 ** 3), grouping=True)
-                    output_str = formatted_size + "GB -> " + item 
+                if directory_size / (1024 ** 3) >= config['min_dir_size']:
+                    output_str = format_gigabytes(directory_size) + "GB -> " + toplevel_subdir 
                     if check_write:
                         output_str += " write:{}".format(write_access)
                     tqdm.write(output_str)
     return directory_sizes
 
 
-def export_to_csv(directory_list, csv_filename):
-    with open(csv_filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Directory', 'Size (bytes)', 'Size (Gigabytes)', 'Write Access'])
-        writer.writerows(directory_list)
-    print(f"Directory list exported to {csv_filename} successfully!")
+def top_level_subdir_names(root_directory, config):
+    _, dirnames, _ = next(os.walk(root_directory))
+    dirnames = [d for d in dirnames if d not in config['ignore_directories']]
+    return dirnames
+
+
+def csv_data_row(config, directory, bytes, write_access):
+    data_row = [directory]
+    if config['report_bytes']: data_row.append(bytes)
+    if config['report_gb']: data_row.append(bytes / (1024 ** 3))
+    if config['check_write']: data_row.append(write_access)
+    return data_row
+
+
+def csv_header_row(config):
+    header_row = ['Directory']
+    if config['report_bytes']: header_row.append('Size (bytes)')
+    if config['report_gb']: header_row.append('Size (Gigabytes)')
+    if config['check_write']: header_row.append('Write Access')
+    return header_row
+
+
+def export_to_csv(directory_list, config):
+    try:
+        with open(config['csv_file'], 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(csv_header_row(config))
+            writer.writerows(directory_list)
+        print(f"Directory list exported to {config['csv_file']} successfully!")
+    except Exception as ex:
+        print(ex)
+
+
+def get_config():
+    config = {}
+    config_reader = ConfigParser()
+    config_reader.read('config.ini')
+    config['root_directory'] = config_reader.get('Directories', 'root_directory')
+    config['ignore_directories'] = config_reader.get('Directories', 'ignore_directories').split(',')
+    config['ignore_directories'] = [directory.strip() for directory in config['ignore_directories']]  
+    config['check_write'] = config_reader.get('Access', 'check_for_write_access').lower() == "true"
+    config['min_dir_size'] = float(config_reader.get('Filters', 'min_dir_size'))
+    config['report_bytes'] = config_reader.get('Reporting', 'bytes').lower() == "true"
+    config['report_gb'] = config_reader.get('Reporting', 'gigabytes').lower() == "true"
+    config['csv_file'] = config_reader.get('Reporting', 'csv_file')
+    config['num_threads'] = int(config_reader.get('Performance', 'num_threads'))
+    return config
 
 
 locale.setlocale(locale.LC_ALL, '')
-logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG, encoding='utf-8')
+logging.basicConfig(filename='error.log', filemode='w', level=logging.DEBUG, encoding='utf-8')
 
-# Read the root_directory from the config.ini file
-config = ConfigParser()
-config.read('config.ini')
-root_directory = config.get('Directories', 'root_directory')
-ignore_directories = config.get('Directories', 'ignore_directories').split(',')
-check_write = config.get('Access', 'check_for_write_access').lower() == "true"
-min_dir_size = float(config.get('Filters', 'min_dir_size'))
-
-# normalise ignore directories to ensure consistent 
-ignore_directories = [directory.strip() for directory in ignore_directories]  
-
-csv_filename = "directory_sizes.csv"
-
-directory_list = get_top_level_dir_sizes(root_directory, ignore_directories, check_write, min_dir_size)
-export_to_csv(directory_list, csv_filename)
+config_ini = get_config()
+directory_list = get_top_level_dir_sizes(config_ini)
+export_to_csv(directory_list, config_ini)
